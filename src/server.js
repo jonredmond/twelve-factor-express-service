4dbccd24708b4386d2142139d01df4aa04869165
Jon Redmond <jredmond@sapient.com>
2019-10-09 15:57:05 +0100

    Add routes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const todos = require('./routes/todos');
+
 const port = process.env.PORT;
+
+app.use('/todos', todos);
 
 app.listen(port, () => {
   console.log('Listening on port: ' + port);
