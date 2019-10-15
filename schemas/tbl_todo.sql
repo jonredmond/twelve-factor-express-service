aab101c77e5ff0f7699b24076e411a7526087c46
Jon Redmond <jredmond@sapient.com>
2019-10-15 12:04:11 +0100

    Add api definitions and sql schemas

---
+++ schemas/tbl_todo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tbl_todo (
+  todo_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user VARCHAR(16) NOT NULL,
+  todo_description VARCHAR(255) NOT NULL,
+  done BOOLEAN NOT NULL,
+  PRIMARY KEY(todo_id)
+);
