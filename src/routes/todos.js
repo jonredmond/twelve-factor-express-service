13bb62f684727685a2f126169ce09e7077c33243
Jon Redmond <jredmond@sapient.com>
2019-10-15 14:37:42 +0100

    Add connection to database with vulnerability

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  const connection = mysql.createConnection({
+    host: '35.246.64.183',
+    user: 'sapient.grads',
+    password: 'sapient2019',
+    database: 'todo_db'
+  });
+
+  connection.connect();
+
+  connection.query(
+    'SELECT * FROM tbl_todo WHERE user="' + req.query.user + '"',
+    (err, rows, fields) => {
+      if (err) {
+        res.sendStatus(500);
+        res.send(err);
+      }
+
+      res.send(rows);
+      connection.end();
+    }
+  );
+});
 router.post('/', (req, res) => {});
 router.put('/:id', (req, res) => {});
 
